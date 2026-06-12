3f2e94a3ece4353151c3afa08f3d87bb096a8c13
Ritik Raushan <raushanritik30891@gmail.com>
2026-06-12 23:32:15 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_BASE_URL = "https://loan-approval-prediction-1-u1ga.onrender.com";
 
 const api = axios.create({
   baseURL: BASE_URL,
